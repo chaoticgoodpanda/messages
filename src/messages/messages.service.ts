4710aa1e5094936024a58d6e8597a83154fe89ed
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-02-02 20:18:38 +0100

    Set up dependency injection

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,18 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  // dependency injection!
+  constructor(public messagesRepo: MessagesRepository) {}
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
